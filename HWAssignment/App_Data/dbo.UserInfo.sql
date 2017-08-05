aac977e6d5d5ffbb4df8aae450e7a6eba4a21368
campbell88 <shatharcampbell@gmail.com>
2017-08-05 13:47:26 -0400

    first commit

---
+++ HWAssignment/App_Data/dbo.UserInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstName] VARCHAR(50) NOT NULL, 
+    [lastName] VARCHAR(50) NOT NULL, 
+    [address1] VARCHAR(100) NOT NULL, 
+    [address2] VARCHAR(100) NULL, 
+    [city] VARCHAR(50) NOT NULL, 
+    [state] NCHAR(2) NOT NULL, 
+    [zip] VARCHAR(9) NOT NULL, 
+    [country] NCHAR(2) NOT NULL
+)
